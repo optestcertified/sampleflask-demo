f1adfdb156a08f3ab9cfa34a7c2e3f77ed04fb60
opy <140424822+optestcertified@users.noreply.github.com>
2023-11-10 10:00:26 +0100

    Port inclusion Update

--- App.py
+++ App.py
@@ -2,7 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
     return "<p>Hello World! It's my time</p>"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
 
